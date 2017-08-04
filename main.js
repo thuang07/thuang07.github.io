41fac9fcff82575579a09b110d1af6bf82f1238d
thuang07 <30581733+thuang07@users.noreply.github.com>
2017-08-04 16:34:35 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ var links = ["https://cache-graphicslib.viator.com/graphicslib/thumbs360x240/525
 
 "https://allwaysdive.com.au/wp-content/uploads/2015/03/Mexico-6.jpg",
 
-"http://www.955170000.com/countries/BRASIL.jpg",
+"https://www.955170000.com/countries/BRASIL.jpg",
 
 "https://s-media-cache-ak0.pinimg.com/736x/46/d9/73/46d973cce29c8c655d07c7519d8e9615--spain-travel-the-architect.jpg",
              
-"http://carmencitta.me/2017/wp-content/uploads/2017/03/Carmencitta-Holland-a-majestic-part-of-Nethelands-5.jpg"
+"https://carmencitta.me/2017/wp-content/uploads/2017/03/Carmencitta-Holland-a-majestic-part-of-Nethelands-5.jpg"
 ];
 var titles = ["NYC","Mexico", "Brazil", "Spain", "Holand"];
 var index = 0;
